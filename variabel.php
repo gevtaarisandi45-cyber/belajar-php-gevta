8879419680cd46b6c9a08bde19be9c1684f777f4
gevtaarisandi45-cyber <gevta.arisandi45@smk.belajar.id>
2025-09-15 13:51:46 +0700

    belajar php variabel dan tipe data

---
+++ variabel.php
@@ -0,0 +1,17 @@
+<?php
+$nama_lengkap = "Gevta Arisandi";
+$umur = 16;
+$alamat = "Pacitan";
+$sudah_lulus = true;
+
+//tampilkan dengan echo
+echo "nama : ".$nama_lengkap."\n";
+echo "umur : ".$umur."\n";
+echo "alamat : ".$alamat."\n";
+echo "sudah_lulus : ".$sudah_lulus."\n";
+
+//tampilkan dengan var_dump
+var_dump($nama_lengkap);
+var_dump($umur);
+var_dump($alamat);
+var_dump($sudah_lulus);
